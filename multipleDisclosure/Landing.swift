8b07a715f62d295f1a63f0848f400a85eaf28335
Chris Wu <31398736+shiftingsand@users.noreply.github.com>
2022-01-17 11:50:24 -0500

    Comment cleanup for readability.

--- multipleDisclosure/Landing.swift
+++ multipleDisclosure/Landing.swift
@@ -7,14 +7,8 @@
 
 import SwiftUI
 
-
-
-
-
 struct Landing: View {
     @EnvironmentObject var allData : AllData
-    //@State var mainList : [Things] = [Things(name: "one", theList:  junk), Things(name: "two", theList: junk), Things(name: "three", theList: junk)]
-   //// @State var myList : [Country] = [Country(name: "USA", animals: [Animals(name: "Turtle", weight: 15), Animals(name: "Cat", weight: 8)]), Country(name: "Canada", animals: [Animals(name: "Moose", weight: 990), Animals(name: "Beaver", weight: 20)]), Country(name: "Germany", animals: [Animals(name: "Chamois", weight: 130), Animals(name: "European Badger", weight: 37)])]
     var body: some View {
         NavigationView {
             ScrollView {
@@ -25,7 +19,6 @@ struct Landing: View {
                     }
                 }
             }
-            //NavigationLink("See animals", destination: ContentView(myList: $myList))
         }
     }
 }
